1f3b6aa979a71e813de26602e61aa5ef6e091ea3
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-05-07 10:04:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,10 @@
+from itertools import combinations
 ary = []
 for _ in range(9):
     ary.append(int(input()))
-ary.sort()
-total_ary = sum(ary)
 
-for i in range(len(ary)):
-    for j in range(i + 1, len(ary)):
-        if total_ary - ary[i] - ary[j] == 100:
-            for k in range(len(ary)):
-                if k == i or k == j:
-                    pass
-                else:
-                    print(ary[k])
-            exit()
+for i in combinations(ary, 7):
+    if sum(i) == 100:
+        for j in sorted(i):
+            print(j)
+        break
