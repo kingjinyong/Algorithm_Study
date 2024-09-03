36bac8686ec78f7d7d727b79e4561fc4df323450
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-09-04 07:07:52 +0900

    [Silver IV] Title: 대칭 차집합, Time: 240 ms, Memory: 81676 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+aset = set(map(int, input().split()))
+bset = set(map(int, input().split()))
+
+print(len(aset-bset) + len(bset-aset))
