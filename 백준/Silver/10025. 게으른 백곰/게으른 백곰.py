0be7d5713a03514afb39d25050e85fd9c773a234
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-09-06 16:05:54 +0900

    [Silver III] Title: 게으른 백곰, Time: 612 ms, Memory: 82592 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
-
+# -----------------
 dic = {}
 findmax = []
 for i in range(n):
@@ -15,28 +15,22 @@ albert = [0 for i in range(m+1)]
 for i in range(len(albert)):
     if i in dic.keys():
         albert[i] = dic[i]
+# ---------------- 각 위치에 맞게 양동이 놓기
 
 a = 0
 b = k*2+1   # 1일 경우: 3, 2일 경우: 5, 3일 경우: 7, 4일 경우: 9
-# print(a, b)
+
 many_ice = []
-# print(sum(albert[a:b]))
 
 many_ice.append(sum(albert[a:b]))
 temp = sum(albert[a:b])
-# print(albert)
-# print(albert[a:b])
-# print(many_ice, a, b)
-# print(temp)
+
 while b < len(albert)-1:
     temp -= albert[a]
     temp += albert[b]
     a += 1
     b += 1
-    # print(a, b)
 
-    # print(temp)
     many_ice.append(temp)
-    # print(many_ice)
 
 print(max(many_ice))
