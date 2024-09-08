0a43a730e10a8b284c16584748101d037d37512b
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-09-08 18:26:23 +0900

    [Silver IV] Title: 30, Time: 68 ms, Memory: 32252 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,17 +1,13 @@
 n = input()
-
-if "0" not in n: # 10의 배수이려면 0이 있어야하는데 없으면 -1 출력
+if "0" not in n: # 10배수라면 0이 있어야 함.
     print(-1)
-
 else:
-    num_sum = 0
+    num = 0
     for i in range(len(n)):
-        num_sum += int(n[i])
+        num += int(n[i])
 
-    if num_sum % 3 != 0: # 3의 배수가 아니라면? -1 출력
+    if num % 3 != 0:    # 3의 배수가 아니라면 -1 출력
         print(-1)
-    # 모든 조건이 충족 되었으니
     else:
-        sorted_num = sorted(n, reverse=True) # 내림차순 정렬하고 
-        answer = "".join(sorted_num)
-        print(answer) # 출력
+        sorted_num = sorted(n, reverse=True)    # 내림차순 정렬해주면서 리스트로 변환
+        print("".join(sorted_num))  # 문자열로 연결해주고 출력
