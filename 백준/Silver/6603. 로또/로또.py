9e9681c914ada0c96bc68fbb9d5a34352b165ad6
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-09-20 16:23:56 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,19 +1,19 @@
 from itertools import combinations
 import sys
 input = sys.stdin.readline
-ary = list(map(int, input().split()))
-rary = []
-for i in range(1, len(ary)):
-    rary.append(ary[i])
-n = ary[0]
+
 while True:
+    ary = list(map(int, input().split()))
+    rary = []
+
+    for i in range(1, len(ary)):
+        rary.append(ary[i])
+
+    n = ary[0]
+
+    if n == 0:
+        break
+
     for i in combinations(rary, 6):
         print(*i)
     print()
-    ary = list(map(int, input().split()))
-    rary = []
-    for i in range(1, len(ary)):
-        rary.append(ary[i])
-    n = ary[0]
-    if n == 0:
-        break
