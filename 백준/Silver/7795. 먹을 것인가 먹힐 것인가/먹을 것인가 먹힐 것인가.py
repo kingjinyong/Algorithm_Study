1bf8b7364ec2f176c71cbb64e6ba42e5a6ae8ac1
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-10-10 21:48:45 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 260 ms, Memory: 34676 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,31 +1,30 @@
-import sys
-input = sys.stdin.readline
 t = int(input())
-for _t in range(t):
+for _ in range(t):
     n, m = map(int, input().split())
     a_ary = list(map(int, input().split()))
     b_ary = list(map(int, input().split()))
     # print(a_ary, b_ary)
     a_ary.sort()
     b_ary.sort()
+
+
     # print(a_ary, b_ary)
 
-
-    def eat(eater):    
-        global result
+    def eat(eater):
         start = 0
         end = len(b_ary) - 1
-
+        c = -1  # 한 쌍도 없을 경우를 위해 -1로 초기화
         while start <= end:
             mid = (start + end) // 2
             if b_ary[mid] < eater:
+                c = mid
                 start = mid + 1
             else:
                 end = mid - 1
-        return len(b_ary[:start])
+        return c + 1
+
 
     cnt = 0
     for i in a_ary:
-        result = 0
         cnt += eat(i)
     print(cnt)
