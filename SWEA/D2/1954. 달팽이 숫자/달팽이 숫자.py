bb1779fd8a8b5f8bc7b82fc8a444a088202ba3e3
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-11-15 16:17:14 +0900

    [D2] Title: 달팽이 숫자, Time: 117 ms, Memory: 43,780 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -2,46 +2,20 @@ test = int(input())
 for t in range(1, test + 1):
     n = int(input())
     ary = [[-1]*n for i in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
 
-    x, y = 0, 0
-    d = 'r'
-    snail = 1
-    while True:
-        if snail == n**2:
-            ary[x][y] = snail
-            break
-        if d == 'r':
-            if y + 1 < n and ary[x][y+1] == -1:
-                ary[x][y] = snail
-                snail += 1
-                y += 1
-            else:
-                ary[x][y] = snail
-                d = 'd'
-        elif d == 'd':
-            if x + 1 < n and ary[x+1][y] == -1:
-                ary[x][y] = snail
-                snail += 1
-                x += 1
-            else:
-                ary[x][y] = snail
-                d = 'l'
-        elif d == 'l':
-            if y - 1 >= 0 and ary[x][y-1] == -1:
-                ary[x][y] = snail
-                snail += 1
-                y -= 1
-            else:
-                ary[x][y] = snail
-                d = 'u'
-        elif d == 'u':
-            if x - 1 < n and ary[x-1][y] == -1:
-                ary[x][y] = snail
-                snail += 1
-                x -= 1
-            else:
-                ary[x][y] = snail
-                d = 'r'
+    x, y, d = 0, 0, 0
+
+    for snail in range(1, n**2+1):
+        ary[x][y] = snail
+        nx, ny = x + dx[d], y+dy[d]
+
+        if not (0 <= nx < n and 0 <= ny < n) or ary[nx][ny] != -1:
+            d = (d + 1) % 4
+            nx, ny = x + dx[d], y + dy[d]
+
+        x, y = nx, ny
 
     print("#", end='')
     print(t)
