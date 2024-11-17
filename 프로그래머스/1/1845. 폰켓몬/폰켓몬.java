6cac627b75a38b74de3d993b1bb84719bcaaf408
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-11-17 17:34:22 +0900

    [level 1] Title: 폰켓몬, Time: 5.79 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Map<Integer, Integer> hs = new HashMap<>();
+        for (int num : nums){
+            if (!hs.containsKey(num)){
+                hs.put(num, 1);
+            }
+            else {
+                hs.put(num, hs.get(num) + 1);
+            }
+        }
+        
+        int compareA = nums.length/2;
+        int compareB = hs.size();
+
+        if (compareB <= compareA){
+            return compareB;
+        }
+        else {
+            return compareA;
+        }
+        }
+        
+    }
