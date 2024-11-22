974392e6b1a25939ae659a949761d997ad7d2010
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-11-22 14:02:03 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,12 +1,19 @@
-t = int(input())
+from itertools import combinations
 
-for i in range(t):
-    n, m = map(int, input().split())
+test = int(input())
 
-    a = 1
-    for j in range(m-n+1, m+1):
-        a *= j
-    b = 1
-    for j in range(1, n+1):
-        b *= j
-    print(a // b)
+for _ in range(test):
+    a, b = map(int, input().split())
+    child = 1
+    parent = 1
+    parent_2 = 1
+
+    for i in range(b, 0, -1):
+        child *= i
+    for i in range(b-a, 0, -1):
+        parent_2 *= i
+    for i in range(a, 0, -1):
+        parent *= i
+        
+    result = child // (parent*parent_2)
+    print(result)
