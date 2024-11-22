8c12c4ffb228dc7e3140218369090f9904bd4fa2
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-11-22 13:41:49 +0900

    [Bronze II] Title: 소수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,21 +1,26 @@
-m = int(input())
+def prime(x, y):
+    prime_number = [True] * 10001
+
+    prime_number[0] = False
+    prime_number[1] = False
+
+    for i in range(2, int(10001 ** 0.5)):
+        if prime_number[i]:
+            for j in range(i*i, 10001, i):
+                    prime_number[j] = False
+
+    result = []
+    for i in range(x, y + 1):
+        if prime_number[i]:
+            result.append(i)
+    return result
+
+
 n = int(input())
+m = int(input())
 
-prime_number = []
-
-def is_prime(a):
-    if a < 2:
-        return False
-    for i in range(2, int(a**0.5)+1):
-        if a % i == 0:
-            return False
-    return True
-
-for i in range(m, n+1):
-        if is_prime(i):
-            prime_number.append(i)
-if len(prime_number) == 0:
-    print(-1)
+if len(prime(n, m)) > 0:
+    print(sum(prime(n, m)))
+    print(min(prime(n, m)))
 else:
-    print(sum(prime_number))
-    print(min(prime_number))
+    print(-1)
