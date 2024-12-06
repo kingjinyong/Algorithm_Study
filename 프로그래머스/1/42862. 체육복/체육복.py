72ea1b501702ac221dca43549b00003c0a4ac523
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-12-06 15:57:42 +0900

    [level 1] Title: 체육복, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,28 @@
+def solution(n, lost, reserve):
+    dic = {}
+
+    for student in range(1, n+1):
+        if student in reserve and student in lost:
+            dic[student] = 1
+        elif student in lost:
+            dic[student] = 0
+        elif student in reserve:
+            dic[student] = 2
+        else:
+            dic[student] = 1
+    print(dic)
+    for n in dic:
+        if n-1 in dic:
+            if dic[n-1] == 0 and dic[n] == 2:
+                dic[n] -= 1
+                dic[n-1] = 1
+        if n+1 in dic:
+            if dic[n+1] == 0 and dic[n] == 2:
+                dic[n] -= 1
+                dic[n+1] = 1
+    result = 0
+    for i in dic.values():
+        if i >= 1:
+            result += 1
+    print(dic)
+    return result
