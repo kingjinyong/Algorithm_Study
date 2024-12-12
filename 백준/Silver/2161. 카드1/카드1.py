64afae52f416800cb35e46b1380986cd9dd66854
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-12-12 23:39:07 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,16 +1,18 @@
 n = int(input())
 ary = []
-ary2 = []
 for i in range(1, n+1):
     ary.append(i)
-s = True
-while len(ary) > 1:
-    if s:
-        ary2.append(ary.pop(0))
-        s = False
-    else:
-        ary.append(ary.pop(0))
-        s = True
+# print(ary)
 
-ary2.append(ary.pop())
-print(*ary2)
+garbage = []
+s = 0
+while len(ary) > 1:
+    # print(garbage)
+    if s == 0:
+        garbage.append(ary.pop(0))
+        s = 1
+        continue
+    elif s == 1:
+        ary.append(ary.pop(0))
+        s = 0
+print(*garbage, *ary)
