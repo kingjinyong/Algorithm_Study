7d14c9d9b9f591c38d2646cbb036f24e3b160564
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-12-27 15:18:44 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,29 +1,14 @@
-import sys
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
-
 ary = list(map(int, input().split()))
-
-a = 0
-b = a + k
-
-sary = []
-
-total = 0
-
-for i in range(b):      # 첫 번째 범위의 합 구하고
-    total += ary[i]
-
-sary.append(total)      # 넣어주고
-
-while b < len(ary):     # 범위 이동하면서 넣어주기
-    total -= ary[a]
-    total += ary[b]
-    
-    sary.append(total)
-
-    a += 1
-    b += 1
-
-print(max(sary))        # 최대값 구하기
+start = 0
+end = k-1
+s_w = sum(ary[:k])
+result = []
+result.append(s_w)
+while end < len(ary)-1:
+    s_w -= ary[start]
+    start += 1
+    end += 1
+    s_w += ary[end]
+    result.append(s_w)
+print(max(result))
