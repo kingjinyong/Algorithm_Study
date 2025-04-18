fe7f212a9d6237e410c8680c1bac0d604e29af17
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2025-04-18 14:58:10 +0900

    [level 2] Title: H-Index, Time: 0.23 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,12 @@
 def solution(citations):
+    # 인용 순으로 논문을 정렬시켜서
     citations.sort()
-
+    print(citations)
+    # 0번부터 시작
     for i in range(len(citations)):
+        # i 번째 논문의 인용 횟수(h)가 논문 개수 - i 즉, h편 이상일 경우 
         if citations[i] >= len(citations) - i:
             return len(citations) - i
-
+        
+    # 해당 되는 부분이 없을 경우
     return 0
