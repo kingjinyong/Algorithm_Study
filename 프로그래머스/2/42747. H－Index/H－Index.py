71d4e1a493224ce220e7f01a1284f8827547ccb8
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2025-04-18 14:40:30 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,13 +1,8 @@
 def solution(citations):
     citations.sort()
-    print(citations)
 
     for i in range(len(citations)):
-        print(citations[i])
         if citations[i] >= len(citations) - i:
-            print(citations[i], len(citations) - i)
             return len(citations) - i
 
     return 0
-
-print(solution([3, 0, 6, 1, 5]))
