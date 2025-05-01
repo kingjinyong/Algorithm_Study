b9285a57488e9526ffed4c9e0856e51b1554d3c5
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2025-05-01 20:17:42 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 78 ms, Memory: 59,008 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -5,11 +5,13 @@ for t in range(1, test + 1):
     n = int(input())
     ary = [0]*10
     c = 1
+    
     while 0 in ary:
         n *= c
         for i in str(n):
             ary[int(i)] = 1
         n //= c
         c += 1
-    n*=(c-1)
+        
+    n *= (c-1)
     print(f"#{t} {n}")
