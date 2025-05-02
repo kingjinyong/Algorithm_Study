4477373a9dd5e5e69082f8aba0b9857a28319fff
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2025-05-02 15:42:23 +0900

    [D2] Title: 초심자의 회문 검사, Time: 66 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,7 +1,18 @@
+from collections import deque
+
 test = int(input())
-for t in range(1, test+1):
-    n = input()
-    if n == n[::-1]:
-        print('#', t, ' ', 1, sep="")
-    else:
-        print('#', t, ' ', 0, sep="")
+
+for t in range(1, test + 1):
+    answer = 0
+    word = input()
+
+    reverse_word_list = list(word)
+    reverse_word_list.reverse()
+
+    reverse_word = ''.join(reverse_word_list)
+
+    # print(word, reverse_word)
+    if word == reverse_word:
+        answer = 1
+
+    print(f"#{t} {answer}")
