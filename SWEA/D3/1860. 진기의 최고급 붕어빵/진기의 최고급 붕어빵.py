2bd866a51000246d4c682fa3f2312c09306c9d3d
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2025-05-23 15:00:43 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 850 ms, Memory: 64,512 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,17 +1,24 @@
-from collections import deque
-test = int(input())  # 테스트 반복 횟수
-for t in range(1, test + 1):
-    n, m, k = map(int, input().split())
-    people_time = list(map(int, input().split()))
-    people_time.sort()
+test = int(input())
 
-    result = ''
-    for i in range(n):
-        boong = people_time[i]//m*k # 해당 시간까지 만든 붕어빵 개수
-        if boong < i+1: # 해당 시간까지 붕어빵을 만든 개수보다 온 손님의 수가 더 많다면..?
-            result = "Impossible"
+for t in range(1, test + 1):
+    N, M, K = map(int, input().split())
+    people = list(map(int, input().split()))
+    dic = {}
+    for i in range(11112):
+        if i in people:
+            dic[i] = people.count(i)
+    fish = 0
+    answer = "Possible"
+    for time in range(1, 11111+1):
+        if 0 in dic.keys():
+            answer = "Impossible"
             break
-        result = "Possible"
-    print("#", end='')
-    print(t, end=' ')
-    print(result)
+        if time % M == 0:
+            fish += K
+        if time in dic.keys():
+            if fish-dic[time] < 0:
+                answer = "Impossible"
+                break
+            fish -= dic[time]
+
+    print(f"#{t} {answer}")
